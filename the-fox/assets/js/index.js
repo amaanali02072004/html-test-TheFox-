75a48f7b990faa6e9763c700865f7abf23bda340
Amaan Ali <killeramaan12345@gmail.com>
2022-06-15 13:45:50 +0530

    external js

---
+++ the-fox/assets/js/index.js
@@ -0,0 +1,305 @@
+const AdLinks = [
+	{
+		title: 'Fully responsive',
+		href: 'javascript:;',
+	},
+]
+const Links = [
+	{
+		title: 'Home',
+		href: 'javascript:;',
+		class: 'active',
+	},
+	{
+		title: 'Pages',
+		href: 'javascript:;',
+		class: '',
+	},
+	{
+		title: 'Portfolio',
+		href: 'javascript:;',
+		class: '',
+	},
+	{
+		title: 'Blog',
+		href: 'javascript:;',
+		class: '',
+	},
+	{
+		title: 'Shop',
+		href: 'javascript:;',
+		class: '',
+	},
+	{
+		title: 'travel',
+		href: 'javascript:;',
+		class: '',
+	},
+	{
+		title: 'shortcodes',
+		href: 'javascript:;',
+		class: '',
+	},
+	{
+		title: 'Features',
+		href: 'javascript:;',
+		class: '',
+	},
+	{
+		title: 'Contact',
+		href: 'javascript:;',
+		class: '',
+	},
+]
+const PortfolioHeadLinks = [
+	{
+		title: 'all',
+		href: 'javascript:;',
+		class: 'large secondaryButton cyan',
+	},
+	{
+		title: 'Web Design',
+		href: 'javascript:;',
+		class: 'large primaryButton',
+	},
+	{
+		title: 'Graphic Design',
+		href: 'javascript:;',
+		class: 'large primaryButton',
+	},
+	{
+		title: 'WordPress',
+		href: 'javascript:;',
+		class: 'large primaryButton',
+	},
+	{
+		title: 'Photography',
+		href: 'javascript:;',
+		class: 'large primaryButton',
+	},
+]
+const FooterWidgetLinks = [
+	{
+		title: 'awesome',
+		href: 'javascript:;',
+		class: 'primaryButton',
+	},
+	{
+		title: 'beautiful',
+		href: 'javascript:;',
+		class: 'primaryButton',
+	},
+	{
+		title: 'flat design',
+		href: 'javascript:;',
+		class: 'primaryButton',
+	},
+	{
+		title: 'ios',
+		href: 'javascript:;',
+		class: 'secondaryButton cyan',
+	},
+	{
+		title: 'themeforest',
+		href: 'javascript:;',
+		class: 'primaryButton',
+	},
+	{
+		title: 'mass',
+		href: 'javascript:;',
+		class: 'primaryButton',
+	},
+]
+const Cards = [
+	{
+		heading: 'Creative Team profile',
+		img: './assets/images/camera.png',
+		alt: 'camera icon',
+		para: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos minima nam praesentium laborum quisquam, consequuntur laboriosam fugiat cupiditate.Nostrum, error? quisquam, consequuntur laboriosam fugiat cupiditate.Nostrum, error?',
+	},
+	{
+		heading: 'Creative Team profile',
+		img: './assets/images/camera.png',
+		alt: 'camera icon',
+		para: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos minima nam praesentium laborum quisquam, consequuntur laboriosam fugiat cupiditate.Nostrum, error? quisquam, consequuntur laboriosam fugiat cupiditate.Nostrum, error?',
+	},
+	{
+		heading: 'Creative Team profile',
+		img: './assets/images/camera.png',
+		alt: 'camera icon',
+		para: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos minima nam praesentium laborum quisquam, consequuntur laboriosam fugiat cupiditate.Nostrum, error? quisquam, consequuntur laboriosam fugiat cupiditate.Nostrum, error?',
+	},
+	{
+		heading: 'Creative Team profile',
+		img: './assets/images/camera.png',
+		alt: 'camera icon',
+		para: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos minima nam praesentium laborum quisquam, consequuntur laboriosam fugiat cupiditate.Nostrum, error? quisquam, consequuntur laboriosam fugiat cupiditate.Nostrum, error?',
+	},
+	{
+		heading: 'Creative Team profile',
+		img: './assets/images/camera.png',
+		alt: 'camera icon',
+		para: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos minima nam praesentium laborum quisquam, consequuntur laboriosam fugiat cupiditate.Nostrum, error? quisquam, consequuntur laboriosam fugiat cupiditate.Nostrum, error?',
+	},
+	{
+		heading: 'Creative Team profile',
+		img: './assets/images/camera.png',
+		alt: 'camera icon',
+		para: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos minima nam praesentium laborum quisquam, consequuntur laboriosam fugiat cupiditate.Nostrum, error? quisquam, consequuntur laboriosam fugiat cupiditate.Nostrum, error?',
+	},
+]
+
+let hUl = document.getElementById('headerLinks')
+let fUl = document.getElementById('footerLinks')
+for (let i = 0; i < Links.length; i++) {
+	hUl.innerHTML += `<li><a title='${Links[i].title}' id='headerLinks${i}' class='${Links[i].class}' onclick='activeLink(this)' href=${Links[i].href}>${Links[i].title}</a></li>`
+	fUl.innerHTML += `<li><a title='${Links[i].title}' id='footerLinks${i}' class='${Links[i].class}' onclick='activeLink(this)' href=${Links[i].href}>${Links[i].title}</a></li>`
+}
+
+let footerWidgets = document.getElementById('footerWidgets')
+for (let i = 0; i < FooterWidgetLinks.length; i++) {
+	footerWidgets.innerHTML += `<li><a title='${FooterWidgetLinks[i].title}' id='FooterWidgetLinks${i}' class='${FooterWidgetLinks[i].class}' onclick='footerActiveLink(this)' href=${FooterWidgetLinks[i].href}>${FooterWidgetLinks[i].title}</a></li>`
+}
+let advertLinks = document.getElementById('adLinks')
+for (let i = 0; i < 40; i++) {
+	advertLinks.innerHTML += `<li><a title='${AdLinks[0].title}' href=${AdLinks[0].href}>${AdLinks[0].title}</a></li>`
+}
+
+let burger = document.getElementById('hamburger')
+let menuList = document.getElementById('rhs')
+function hideMenu() {
+	menuList.classList.remove('rhsActive')
+}
+function showMenu() {
+	menuList.classList.toggle('rhsActive')
+}
+
+let banner = document.getElementById('banner')
+let bannerPosition = 0
+let bannerLengthMin = -1
+let bannerLengthMax = 4
+let seconds = 5
+let bannerDuration = seconds * 1000
+let timer = setInterval(function () {
+	changeBanner2()
+}, bannerDuration)
+function changeBanner0() {
+	if (bannerPosition == bannerLengthMin) {
+		bannerPosition = bannerLengthMax
+	}
+	bannerPosition--
+	banner.classList = `bannerOuterWrapper banner${bannerPosition}`
+}
+function changeBanner2() {
+	bannerPosition++
+	if (bannerPosition == bannerLengthMax) {
+		bannerPosition = bannerLengthMin
+	}
+	banner.classList = `bannerOuterWrapper banner${bannerPosition}`
+}
+
+let headerActiveLinks = [],
+	footerActiveLinks = []
+for (var i = 0; i <= Links.length; ++i) {
+	headerActiveLinks[i] = document.getElementById(`headerLinks${i}`)
+	footerActiveLinks[i] = document.getElementById(`footerLinks${i}`)
+}
+function activeLink(element) {
+	for (let i = 0; i < Links.length; i++) {
+		headerActiveLinks[i].classList = ''
+		footerActiveLinks[i].classList = ''
+		element.classList.add('active')
+		if (headerActiveLinks[i].classList == 'active') {
+			footerActiveLinks[i].classList.add('active')
+		} else if (footerActiveLinks[i].classList == 'active') {
+			headerActiveLinks[i].classList.add('active')
+		}
+	}
+}
+
+let cardsSection = document.getElementById('cardsSectionInnerWrapper')
+
+for (let i = 0; i < Cards.length; i++) {
+	cardsSection.innerHTML += `<div class="cardWrapper">
+					<div class="cardHeadWrapper">
+						<img src=${Cards[i].img} alt='${Cards[i].alt}'>
+						<h5>${Cards[i].heading}</h5>
+					</div>
+					<p>${Cards[i].para}</p>
+				</div>`
+}
+let uhsHidden = document.getElementById('uhsHidden')
+function loadMore() {
+	uhsHidden.classList.remove('hidden')
+}
+
+let portfolioHead = document.getElementById('portfolioHeadLinks')
+for (let i = 0; i < PortfolioHeadLinks.length; i++) {
+	portfolioHead.innerHTML += `<a title='${PortfolioHeadLinks[i].title}' id='portfolioLink${i}' class='${PortfolioHeadLinks[i].class}' onclick='portfolioActiveLink(this)' href=${PortfolioHeadLinks[i].href}>${PortfolioHeadLinks[i].title}</a>`
+}
+let portfolioLink = []
+for (var i = 0; i < PortfolioHeadLinks.length; ++i) {
+	portfolioLink[i] = document.getElementById(`portfolioLink${i}`)
+}
+function portfolioActiveLink(element) {
+	for (let i = 0; i < PortfolioHeadLinks.length; i++) {
+		portfolioLink[i].classList = 'large primaryButton'
+	}
+	element.classList = 'large secondaryButton cyan'
+}
+
+let footerWidgetLink0 = document.getElementById('FooterWidgetLinks0')
+let footerWidgetLink1 = document.getElementById('FooterWidgetLinks1')
+let footerWidgetLink2 = document.getElementById('FooterWidgetLinks2')
+let footerWidgetLink3 = document.getElementById('FooterWidgetLinks3')
+let footerWidgetLink4 = document.getElementById('FooterWidgetLinks4')
+let footerWidgetLink5 = document.getElementById('FooterWidgetLinks5')
+function footerActiveLink(element) {
+	footerWidgetLink0.classList = 'primaryButton'
+	footerWidgetLink1.classList = 'primaryButton'
+	footerWidgetLink2.classList = 'primaryButton'
+	footerWidgetLink3.classList = 'primaryButton'
+	footerWidgetLink4.classList = 'primaryButton'
+	footerWidgetLink5.classList = 'primaryButton'
+	element.classList = 'secondaryButton cyan'
+}
+
+let Name = document.getElementById('Name')
+let Email = document.getElementById('Email')
+let Message = document.getElementById('Message')
+
+Name.addEventListener('keypress', function (event) {
+	if (event.key === 'Enter') {
+		document.getElementById('Submit').click()
+	}
+})
+Email.addEventListener('keypress', function (event) {
+	if (event.key === 'Enter') {
+		document.getElementById('Submit').click()
+	}
+})
+// Message.addEventListener("keypress", function (event) {
+// 	if (event.key === "Enter") {
+// 		document.getElementById("Submit").click();
+// 	}
+// });
+function submitForm() {
+	if (Name.value == '') {
+		alert('Enter Your Name!')
+		Name.focus()
+	} else if (Email.value == '') {
+		alert('Enter Your Email!')
+		Email.focus()
+	} else if (Message.value == '') {
+		alert('Enter Your Message!')
+		Message.focus()
+	} else {
+		alert('Your Form has been submitted')
+		console.log(`Name: ${Name.value},
+${Email.value},
+${Message.value}`)
+		Name.value = ''
+		Email.value = ''
+		Message.value = ''
+	}
+}
