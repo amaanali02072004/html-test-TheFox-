15b2c6538cef8ba7f9b84ef444ea7fe6c10e0873
Amaan Ali <killeramaan12345@gmail.com>
2022-06-27 16:50:08 +0530

    batch file

---
+++ push.bat
@@ -0,0 +1,30 @@
+@echo off
+cls
+Color 0C
+title GitHub Pushing
+set /p COM=Commit message: 
+git add *
+git commit -m "%COM%"
+git pull origin main
+git push origin main
+git status
+@echo ----------------------------------------------------------------------------------------
+
+Color 0A
+@echo Pushed and Committed succesfully (%COM%)
+
+@echo ----------------------------------------------------------------------------------------
+setlocal
+:PROMPT
+SET /P EXIT=Exit? (y/[n])
+IF /I "%EXIT%" NEQ "Y" GOTO EEND
+
+exit
+:EEND
+SET /P RETRY=Retry? (y/[n])
+IF /I "%RETRY%" NEQ "Y" GOTO REND
+push
+:REND
+
+
+endlocal
