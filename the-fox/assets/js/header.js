f2f63005510b92cf8d4abf1655a7275b6cbe2bf9
Amaan-Ali-Prodige <amaan.ali@prodigedigital.com>
2022-07-29 16:12:34 +0530

    significant changes

--- the-fox/assets/js/header.js
+++ the-fox/assets/js/header.js
@@ -1,10 +1,10 @@
 let header = document.getElementById('header')
-
+let indexLink = './'
 header.innerHTML = `
 		<div class="container">
 			<div class="headerWrapper">
 				<div class="lhs">
-					<a href="javascript:;" title="thefox">
+					<a href="${indexLink}" title="thefox">
 						<img src="./assets/images/fox-logo.png" class="logo whiteLogo" alt="Logo">
 						<img src="./assets/images/fox-logo-black.png" class="logo blackLogo" alt="Logo">
 					</a>
@@ -14,15 +14,6 @@ header.innerHTML = `
 				</div>
 				<div class="rhs" id="rhs">
 					<ul id="headerLinks">
-						<li><a href="#">Home</a></li>
-						<li><a href="#">Pages</a></li>
-						<li><a href="#">Portfolio</a></li>
-						<li><a href="#">Blog</a></li>
-						<li><a href="#">Shop</a></li>
-						<li><a href="#">Home</a></li>
-						<li><a href="#">Home</a></li>
-						<li><a href="#">Features</a></li>
-						<li><a href="#">Contact</a></li>
 					</ul>
 					<img
 						src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Search_Icon.svg/2048px-Search_Icon.svg.png"
