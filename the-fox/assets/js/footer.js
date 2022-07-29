55a3101c762b7bc9373e14f93e86a553f2d275a9
Amaan-Ali-Prodige <amaan.ali@prodigedigital.com>
2022-07-29 10:50:49 +0530

    added footer component

---
+++ the-fox/assets/js/footer.js
@@ -0,0 +1,113 @@
+let footer = document.getElementById('footer')
+footer.setAttribute('onclick', 'hideMenu(this)')
+footer.innerHTML = `
+		<div class="primaryFooterOuterWrapper">
+			<div class="container">
+				<div class="primaryFooterInnerWrapper">
+					<div>
+						<div class="footerSection section1">
+							<img src="./assets/images/fox-logo.png" alt="logo">
+							<div class="lhs" id="fLhs">
+								<p><i>loading...</i></p>
+								<p><i>loading...</i></p>
+								<ul>
+									<li>
+										<a><i>loading...</i></a>
+									</li>
+								</ul>
+							</div>
+							<div class="rhs" id="fRhs">
+								<i>loading...</i>
+							</div>
+						</div>
+						<div class="footerSection section2" id="fs2">
+							<h5><i>loading...</i></h5>
+							<div class="blog">
+								<div class="blogLhs">
+									<img src="" alt="blog img">
+								</div>
+								<div class="blogRhs">
+									<p class="title"><i>loading...</i></p>
+									<p><i>loading...</i></p>
+									<p><i>loading...</i></p>
+								</div>
+							</div>
+							<div class="blog">
+								<div class="blogLhs">
+									<img src="" alt="blog img">
+								</div>
+								<div class="blogRhs">
+									<p class="title"><i>loading...</i></p>
+									<p><i>loading...</i></p>
+									<p><i>loading...</i></p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div>
+						<div class="footerSection section3">
+							<h5 id="footerWidgetsTitle"><i>loading...</i></h5>
+							<div class="footerWidgets">
+								<ul id="footerWidgets">
+									<li>
+										<a><i>loading...</i></a>
+									</li>
+									<li>
+										<a><i>loading...</i></a>
+									</li>
+									<li>
+										<a><i>loading...</i></a>
+									</li>
+								</ul>
+							</div>
+							<h5 id="footerUsefulLinksTitle"><i>loading...</i></h5>
+							<div class="footerLinks">
+								<ul id="footerUsefulLinks">
+									<li>
+										<i>loading...</i>
+									</li>
+									<li>
+										<i>loading...</i>
+									</li>
+									<li>
+										<i>loading...</i>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<div class="footerSection section4" id="fs4">
+							<h5><i>loading...</i></h5>
+							<div class="contact">
+								<p class="title"><i>loading...</i></p>
+								<p><i>loading...</i></p>
+								<p><i>loading...</i></p>
+							</div>
+							<div class="contact">
+								<p class="title"><i>loading...</i></p>
+								<p><i>loading...</i></p>
+								<p><i>loading...</i></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="secondaryFooterOuterWrapper">
+				<div class="container">
+					<div class="secondaryFooterInnerWrapper">
+						<div class="lhs">
+							<p>Copyright
+							<p id="__date" style="margin: 0 4px;">2014</p> <a href="https://github.com/Killer-Amaan-Ali/html-test"
+								target="_blank">the<b>fox</b></a></p>&nbsp|&nbsp
+							<p>All rights reserved</p>&nbsp|&nbsp
+							<p>designed by <a href="https://github.com/Killer-Amaan-Ali/" target="_blank">Amaan</a></p>
+						</div>
+						<div class="rhs">
+							<ul id="footerLinks">
+								<li><a><i>loading...</i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+`
